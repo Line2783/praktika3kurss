3855475cd0e401593855ec7d0d25e42910678f28
Sergey Shatrov <2003df@maul.ru>
2022-05-23 00:14:03 +0300

    добавил роли

--- WorkWithAbonent/MainWindow.xaml.cs
+++ WorkWithAbonent/MainWindow.xaml.cs
@@ -24,5 +24,80 @@ namespace WorkWithAbonent
         {
             InitializeComponent();
         }
+        private void UsersComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (userList[UsersComboBox.SelectedIndex].Роль)
+            {
+                case "Руководитель отдела по работе с клиентами":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Collapsed;
+                    AssetsTextBox.Visibility = Visibility.Collapsed;
+                    BillingTextBox.Visibility = Visibility.Visible;
+                    SupprtTextBox.Visibility = Visibility.Collapsed;
+                    crmTextBox.Visibility = Visibility.Visible;
+
+                    break;
+
+                case "Менеджер по работе с клиентами\r\n":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Collapsed;
+                    AssetsTextBox.Visibility = Visibility.Collapsed;
+                    BillingTextBox.Visibility = Visibility.Collapsed;
+                    SupprtTextBox.Visibility = Visibility.Collapsed;
+                    crmTextBox.Visibility = Visibility.Visible;
+
+                    break;
+
+                case "Руководитель отдела технической поддержки\r\n":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Visible;
+                    AssetsTextBox.Visibility = Visibility.Collapsed;
+                    BillingTextBox.Visibility = Visibility.Collapsed;
+                    SupprtTextBox.Visibility = Visibility.Visible;
+                    crmTextBox.Visibility = Visibility.Visible;
+
+                    break;
+
+                case "Специалист ТП (выездной инженер)\r\n":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Visible;
+                    AssetsTextBox.Visibility = Visibility.Collapsed;
+                    BillingTextBox.Visibility = Visibility.Collapsed;
+                    SupprtTextBox.Visibility = Visibility.Visible;
+                    crmTextBox.Visibility = Visibility.Visible;
+
+                    break;
+
+                case "Бухгалтер\r\n":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Collapsed;
+                    AssetsTextBox.Visibility = Visibility.Visible;
+                    BillingTextBox.Visibility = Visibility.Visible;
+                    SupprtTextBox.Visibility = Visibility.Collapsed;
+                    crmTextBox.Visibility = Visibility.Collapsed;
+
+                    break;
+
+                case "Директор по развитию\r\n":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Visible;
+                    AssetsTextBox.Visibility = Visibility.Visible;
+                    BillingTextBox.Visibility = Visibility.Visible;
+                    SupprtTextBox.Visibility = Visibility.Visible;
+                    crmTextBox.Visibility = Visibility.Visible;
+
+                    break;
+
+                case "Технический департамент\r\n":
+                    SubsTextBox.Visibility = Visibility.Visible;
+                    EquipTextBox.Visibility = Visibility.Visible;
+                    AssetsTextBox.Visibility = Visibility.Visible;
+                    BillingTextBox.Visibility = Visibility.Collapsed;
+                    SupprtTextBox.Visibility = Visibility.Visible;
+                    crmTextBox.Visibility = Visibility.Visible;
+
+                    break;
+            }
+        }
     }
 }
